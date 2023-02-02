42bc5176da6e23e4dcdec7c0f5b72a5cf12b2345
benj-lazaro <benjie.work@gmail.com>
2023-02-02 17:18:51 +0800

    Section 18 2nd coding exercise return_day initial commit

--- 18-functions-exercises/02-coding-exercise-return-day.py
+++ 18-functions-exercises/02-coding-exercise-return-day.py
@@ -1,27 +1,18 @@
 # Write a function called return_day
 # Takes 1 parameter = a number from 1 to 7
-# Returns the day of the week; 1 = Sunday
+# Returns the day of the week; 1 = Sunday ... 7 = Saturday
 # If number i is less than 1 or greater than 7, return None
 
 def return_day(number):
-    """return_day(number) accepts an integer between 1 to 7 & returns the day of week (1 = Sunday, 2 = Monday, etc.)"""
-    if number == 1:
-        return "Sunday"
-    elif number == 2:
-        return "Monday"
-    elif number == 3:
-        return "Tuesday"
-    elif number == 4:
-        return "Wednesday"
-    elif number == 5:
-        return "Thursday"
-    elif number == 6:
-        return "Friday"
-    elif number == 7:
-        return "Saturday"
+    """return_day(number) accepts an integer between 1 to 7 & returns the day of week (1 = Sunday ... 7 = Saturday)"""
+    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    if number > 0 and number < len(days):
+        return days[number - 1]
     return None
 
 print("Function: return_day()")
 print(f"Documentation: {return_day.__doc__}")
+
 user_input = int(input("Enter number of day: "))
 print(f"You have chosen: {return_day(user_input)}")
