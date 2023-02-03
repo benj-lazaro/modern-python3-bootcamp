9a1c9ecd8e254b27900472ef757163440e158247
benj-lazaro <benjie.work@gmail.com>
2023-02-03 15:40:54 +0800

    Section 18 11th coding exercise compact initial commit

---
+++ 18-functions-exercises/12-coding-exercise-compact.py
@@ -0,0 +1,14 @@
+# Write a function called compact
+# Accepts 1 parameter (list)
+# Returns a list of values that are truthy only
+
+def compact(list_data):
+    """compact(list) returns a list of truthy values only."""
+    return [item for item in list_data if item]
+
+
+list_sample = [0, 1, 2, "", [], False, {}, None, "All done"]
+print("Function: compact()")
+print(f"Documentation: {compact.__doc__}")
+print(f"list_sample: {list_sample}")
+print(f"Result: {compact(list_sample)}")
