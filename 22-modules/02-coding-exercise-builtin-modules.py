dec8a9917130738c49f2e38c679f8e644dc48d5a
benj-lazaro <benjie.work@gmail.com>
2023-02-13 16:34:13 +0800

    Section 22 coding exercise built-in modules initial commit

---
+++ 22-modules/02-coding-exercise-builtin-modules.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+# Write a program that imports the math module
+# Use the math.sqrt() to find the square root of 15129
+# Save the result to a variable called answer
+
+given_number = 15129
+answer = sqrt(given_number)
+print(f"The square root of {given_number} is {answer}")
