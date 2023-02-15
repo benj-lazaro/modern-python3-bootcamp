107811d45c0cbb8b7405d246068fdc4d727d6e36
benj-lazaro <benjie.work@gmail.com>
2023-02-15 23:56:58 +0800

    Section 23 first request initial commit

---
+++ 23-http-requests/02-first-request.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "https://www.google.com"
+response = requests.get(url)
+
+print(f"Your request to {url} came back with status code {response.status_code}.")
+print(f"Content: {response.text}")
