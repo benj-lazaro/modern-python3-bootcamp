b5b94bef299d0e10c658dd526b87be6b435ac756
benj-lazaro <benjie.work@gmail.com>
2023-02-16 14:48:47 +0800

    Section 24 create class and instance, coding exercise initial commit

---
+++ 24-oop-01/01-create-class-instance.py
@@ -0,0 +1,10 @@
+# Define a simple Python class
+class User:
+    pass
+
+
+# Instantiate an object from the User class & assign it into a variable user1
+user1 = User()
+
+print(user1)        # confirms the creation of a User object & it's current place in memory
+print(type(user1))  # identifies the class name that the object in variable user1 instantiated from
