da076d4ba7a0a4c59e09b35e2e93fc7736f3bf0b
benj-lazaro <benjie.work@gmail.com>
2023-02-20 15:15:53 +0800

    Section 25 Deck of Cards (DoC) Card class add code comments

--- 25-deck-of-cards-exercise/01-doc-card-class.py
+++ 25-deck-of-cards-exercise/01-doc-card-class.py
@@ -1,3 +1,8 @@
+# Implement a class called Card
+# Each instance should have a suit (Hearts, Diamonds, Clubs or Spades)
+# Each instance should have a value (2 to 10, A, K, Q, or J)
+# The __repr__() should display the card's value and suit (e.g. A of Hearts)
+
 class Card:
     def __init__(self, value, suit):
         # Instance Attribute(s)
