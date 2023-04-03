ac2c8d625307f3ae4cae38d8028189889c6003c2
benj-lazaro <benjie.work@gmail.com>
2023-04-04 01:29:27 +0800

    Section 35 Python+SQL inserting initial commit

---
+++ 35-python-sql/02-inserting/bulk_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO dogs (name, age, breed) VALUES ("Champ", 4, "Husky");
+INSERT INTO dogs (name, age, breed) VALUES ("Rose", 11, "Chihuahua");
+INSERT INTO dogs (name, age, breed) VALUES ("Moose", 5, "Lab");
+INSERT INTO dogs (name, age, breed) VALUES ("Piggy", 9, "Corgi");
