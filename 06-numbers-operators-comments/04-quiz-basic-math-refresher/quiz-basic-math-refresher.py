2ac462f86ec9a7e2df6fc06bb65a5a74ef4bb49f
benj-lazaro <benjie.work@gmail.com>
2023-09-27 22:06:09 +0800

    Section 06 quiz basic math refresher

--- 06-numbers-operators-comments/04-quiz-basic-math-refresher/quiz-basic-math-refresher.py
+++ 06-numbers-operators-comments/04-quiz-basic-math-refresher/quiz-basic-math-refresher.py
@@ -1,8 +1,9 @@
 # What is the result of the following expression? 2 + 10 * 5 + 3 * 2
-print(2 + 10 * 5 + 3 * 2)
+print(2 + 10 * 5 + 3 * 2)   # Evaluates to 58
 
 # How can we add parenthesis to the following expression to make it = 100? 1 + 9 * 100
-print((1 + 9) * 10)
+print((1 + 9) * 10)         # Evaluates to 100
 
 # What is the result of the following expression? 6 * 8 / 2**3 - 2 * 2
-print(6 * 8 / 2**3 - 2 * 2)
+print(6 * 8 / 2**3 - 2 * 2) # Evaluates to 2.0
+
