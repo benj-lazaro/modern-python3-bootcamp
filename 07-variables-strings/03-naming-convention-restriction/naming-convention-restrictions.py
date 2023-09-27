2f7ca74e706b6502c920d2607f1931e0aec80311
benj-lazaro <benjie.work@gmail.com>
2023-09-27 23:07:08 +0800

    Section 07 variable naming convention and restrictions content refactor

--- 07-variables-strings/03-naming-convention-restriction/naming-convention-restrictions.py
+++ 07-variables-strings/03-naming-convention-restriction/naming-convention-restrictions.py
@@ -1,6 +1,4 @@
 # Variable Naming Restrictions
-# ============================
-
 # Variable name must start with a letter or underscore
 _cats = "Felix"
 print(_cats)
@@ -12,22 +10,21 @@ print(cats2)
 # Variable names are case-sensitive
 CATS = "Snuggles"
 print(CATS)
-print(cats)     # Triggers a NameError since the variable cats is not defined
+print(cats)  # Triggers a NameError since the variable cats is not defined
 
 
 # Variable Naming Conventions
-# ===========================
-
 # Most variable names uses snake_case & lowercase
 cat_name = "put-put"
 
 # A constant variable name uses captal snake_case
 PI_number = 3.1415
 
+
 # A class name uses UpperCamelCase
 class ClassName:
     pass
 
+
 # A dunder variable starts and ends with two underscore characters
 __no_touchy__
-
