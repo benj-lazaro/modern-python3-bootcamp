dc42f07b116c495f8388a1572ab80386b5a9c5fe
benj-lazaro <benjie.work@gmail.com>
2023-09-27 22:57:57 +0800

    Section 07 code exercise bank money robbery code refactor

--- 07-variables-strings/02-coding-exercise-bank-money-robbery.py
+++
@@ -1,7 +0,0 @@
-#  We got away with $19,867,324,678,987.99, and now we have to split it up 5 ways.
-# Using the cash variable, print out the dollar amount each robber gets to keep
-
-cash = 19867324678987.99  # DON'T CHANGE THE CASH VARIABLE
-
-# ADD YOUR CODE BELOW:
-print(cash/5)
