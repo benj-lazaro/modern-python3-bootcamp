e6721339eaf92060344d46e0ee6157d2fd3bbc56
benj-lazaro <benjie.work@gmail.com>
2023-10-01 14:29:07 +0800

    Section 07 special value none content refactor

---
+++ 07-variables-strings/07-special-value-none/07-the-special-value-none.py
@@ -0,0 +1,14 @@
+# The special value None; Python's version of null
+
+name = "Daisy"
+age = 30
+number_of_children = None
+
+print(type(name))                   # Returns str
+print(name)
+
+print(type(age))                    # Returns int
+print(age)
+
+print(type(number_of_children))     # Returns NoneType
+print(number_of_children)
