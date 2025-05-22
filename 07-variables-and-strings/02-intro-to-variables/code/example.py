7effaa053cd948a8494a40e85747f528c608b3f4
benj-lazaro <benjie.work@gmail.com>
2025-05-22 19:36:01 +0800

    02 introduction to variables

---
+++ 07-variables-and-strings/02-intro-to-variables/code/example.py
@@ -0,0 +1,28 @@
+# Variables
+
+# Variable assignment
+x = 100
+khaleesi_mother_of_dragons = 1
+print(khaleesi_mother_of_dragons + x)
+
+# Variable assignment to another
+current_cash = 100
+my_wallet = current_cash
+print(current_cash)
+print(my_wallet)
+
+# Variable value re-assignment
+current_value = 12
+print(current_value)
+
+current_value = "It's me a Mario!"
+print(current_value)
+
+current_value = True
+print(current_value)
+
+# Multiple variables assignment
+user1, user2, user3 = "george", "michael", "jackson"
+print(user1)
+print(user2)
+print(user3)
