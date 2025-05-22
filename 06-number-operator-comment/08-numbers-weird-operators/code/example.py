1c6450814b89f5f6bfef845be23bc510a77d64f4
benj-lazaro <benjie.work@gmail.com>
2025-05-22 17:13:29 +0800

    08 numbers weird operators

---
+++ 06-number-operator-comment/08-numbers-weird-operators/code/example.py
@@ -0,0 +1,14 @@
+# Weird Operators
+
+# Exponentiation
+print(2 ** 3)
+print(49 ** 0.5)
+
+# Modulo
+print(10 % 3)
+print(25 % 4)
+print(16 % 2)
+
+# Integer division
+print(10 // 3)
+print(10 / 3) # Normal division operation
