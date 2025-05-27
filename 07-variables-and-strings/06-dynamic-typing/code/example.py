7209b43b3335d3c066e13f7a4bdaf900cf9f0c36
benj-lazaro <benjie.work@gmail.com>
2025-05-27 21:30:24 +0800

    05 what the heck is dynamic typing

---
+++ 07-variables-and-strings/06-dynamic-typing/code/example.py
@@ -0,0 +1,17 @@
+# Dynamic Typing
+
+# Initially assigned the variable with a boolean value
+awesomeness = True
+print(awesomeness)
+
+# Then to a string
+awesomeness = "a dog"
+print(awesomeness)
+
+# Then to a none (similar to JavaScript's null)
+awesomeness = None
+print(awesomeness)
+
+# Lastly to a float
+awesomeness = 22 / 7
+print(awesomeness)
