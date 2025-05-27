77e4a24ba114342351de36f2729614fdccb6a84c
benj-lazaro <benjie.work@gmail.com>
2025-05-27 20:51:54 +0800

    05 data types

---
+++ 07-variables-and-strings/05-data-types/code/example.py
@@ -0,0 +1,24 @@
+# Data Types
+
+# bool
+is_true = True
+is_game_over = False
+print(is_true)
+print(type(is_true))
+print(is_game_over)
+print(type(is_game_over))
+
+# str
+some_string = "This is a string data type"
+print(some_string)
+print(type(some_string))
+
+# list
+numbers = [1, 2, 3]
+print(numbers)
+print(type(numbers))
+
+# dict
+gamer_bio = {"login_name": "playerOne", "first_name": "Thomas", "last_name": "Anderson"}
+print(gamer_bio)
+print(type(gamer_bio))
